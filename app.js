f9489a318cf45f790e4b6c22baa5859491b38f5c
donMayor <mayowaegbewunmi@gmail.com>
2017-02-14 17:02:40 +0100

    Encryption and decryption in node js

---
+++ app.js
@@ -0,0 +1,78 @@
+var crypto  = require('crypto');
+var fs  = require('fs');
+
+var algorithm =  'aes-128-cbc';
+
+var key = new Buffer([1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6]); //sample encryption key
+var iv = new Buffer([1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6]);  //initialization vector
+
+
+function encryptThenDecrypt(inputFilePath, encryptedFilePath, decryptedFilePath ){
+
+    doEncryption(inputFilePath, encryptedFilePath, function (err) {
+        if(err) return console.log(err);
+
+        console.log("Encryption completed");
+
+        doDecryption(encryptedFilePath, decryptedFilePath, function(err){
+            if(err) return console.log(err);
+
+            console.log("Decryption completed");
+        })
+    })
+}
+
+function doEncryption(inputPath, outputPath, callback){
+    var cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    var inputStream = fs.createReadStream(inputPath);
+    var outputStream = fs.createWriteStream(outputPath);
+
+    inputStream.on('data', function(data) {
+        var buf = new Buffer(cipher.update(data), 'binary');
+        outputStream.write(buf);
+    });
+
+    inputStream.on('end', function() {
+        try {
+            var buf = new Buffer(cipher.final('binary'), 'binary');
+            outputStream.write(buf);
+            outputStream.end();
+            outputStream.on('close', function() {
+                return callback();
+            });
+        } catch(e) {
+            fs.unlink(outputPath);
+            return callback(e);
+        }
+    });
+}
+
+function doDecryption(inputPath, outputPath, callback){
+    var decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+    var inputStream = fs.createReadStream(inputPath);
+    var outputStream = fs.createWriteStream(outputPath);
+
+    inputStream.on('data', function(data) {
+        var buf = new Buffer(decipher.update(data), 'binary');
+        outputStream.write(buf);
+    });
+
+    inputStream.on('end', function() {
+        try {
+            var buf = new Buffer(decipher.final('binary'), 'binary');
+            outputStream.write(buf);
+            outputStream.end();
+            outputStream.on('close', function() {
+                return callback();
+            });
+        } catch(e) {
+            fs.unlink(outputPath);
+            return callback(e);
+        }
+    });
+}
+
+encryptThenDecrypt('/Users/mayowa/Documents/npvn-content/video.mp4', '/Users/mayowa/Documents/npvn-content/encrypted.video.dat', '/Users/mayowa/Documents/npvn-content/output.video.mp4');
+
